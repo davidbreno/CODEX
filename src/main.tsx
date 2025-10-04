18178e00ade361a8c40f3456ab1fc2fe1f63cf24
davidbreno <dr.davidbreno@hotmail.com>
2025-10-04 10:26:50 -0300

    Clean merge artifacts from main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
- codex/implement-usetheme-hook-and-context
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { ThemeProvider } from './theme';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
-
- codex/create-kpi-cards-with-recharts
-
- codex/create-dashboardlayout-components-and-routing
- dev
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
- codex/create-kpi-cards-with-recharts
-
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
- dev
- dev
- dev
   </React.StrictMode>
 );
